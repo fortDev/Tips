ee0280969401afbd5588c07129aa71fc18b6b85a
namomalencar <namomalencar@gmail.com>
2017-12-09 10:49:05 -0300

    Appending Strings

--- src/string/StringConcat.java
+++ src/string/StringConcat.java
@@ -2,33 +2,50 @@ package string;
 
 public class StringConcat {
 
+	private static final int size = 40000;
+
+
 	public static void main(String[] args) {
-		System.out.println("Adding string: " + calcTime("adding"));
-		System.out.println("Append string: " + calcTime("append"));
+		
+		System.out.println("Append, Append string: " + calcTime("appendAppend"));
+		System.out.println("Append, Adding string: " + calcTime("appendAdding"));
+		System.out.println("Adding, Adding string: " + calcTime("addingAdding"));
 	}
 
-	// Concating string on a new object
-	private static void adding() {
+	//Concating string on a new object
+	private static void addingAdding() {
 		String s1 = "Concated Numbers: ";
 		String s2 = "";
 		String s3 = "";
-		for (int i = 0; i <= 50000; i++) {
+		for (int i = 0; i <= size; i++) {
 			s2 = s2 + i + ", ";
 		}
 		s3 = s1 + s2;
-		// System.out.println(s3);
+		//System.out.println(s3);
 	}
 
-	// Concating string on the same object
-	private static void append() {
+	//Concating string on the same object
+	private static void appendAdding() {
 		String s1 = "Concated Numbers: ";
 		StringBuilder s2 = new StringBuilder();
 		String s3 = "";
-		for (int i = 0; i <= 50000; i++) {
+		for (int i = 0; i <= size; i++) {
 			s2.append(i + ", ");
 		}
 		s3 = s1 + s2;
-		// System.out.println(s3);
+		//System.out.println(s3);
+	}
+	
+	//Concating string on the same object
+	private static void appendAppend() {
+		String s1 = "Concated Numbers: ";
+		StringBuilder s2 = new StringBuilder();
+		String s3 = "";
+		for (int i = 0; i <= size; i++) {
+			s2.append(i).append(", ");
+		}
+		s3 = s1 + s2;
+		//System.out.println(s3);
 	}
 
 	private static String calcTime(String s) {
@@ -39,11 +56,14 @@ public class StringConcat {
 		return (totalTime + "ms");
 	}
 
+	
 	private static void concatChoice(String s) {
-		if (s.equals("adding"))
-			adding();
-		if (s.equals("append"))
-			append();
+		if (s.equals("addingAdding"))
+			addingAdding();
+		if (s.equals("appendAdding"))
+			appendAdding();
+		if (s.equals("appendAppend"))
+			appendAppend();
 	}
 
 }
